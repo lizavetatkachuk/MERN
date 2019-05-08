02776578eae299a11407c76017d6ebde08445f0a
Lizaveta <elis.tkachuk@gmailcom>
2019-05-08 20:45:04 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.handleModalView = this.handleModalView.bind(this);
+    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
